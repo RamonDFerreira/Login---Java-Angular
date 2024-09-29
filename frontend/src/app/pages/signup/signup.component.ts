fa2a6bf751e399b8f756e228bd48956e8744d8d5
Ramon Ferreira <ramondiegoaf@hotmail.com>
2024-09-29 20:09:04 -0300

    FrontEnd completo

---
+++ frontend/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+
+interface SignupForm {
+  name: FormControl,
+  email: FormControl,
+  password: FormControl,
+  passwordConfirm: FormControl
+}
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
+  providers: [
+    LoginService
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignUpComponent {
+  signupForm!: FormGroup<SignupForm>;
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ){
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+    })
+  }
+
+  submit(){
+    this.loginService.signup(this.signupForm.value.name, this.signupForm.value.email, this.signupForm.value.password, this.signupForm.value.passwordConfirm).subscribe({
+      next: () => {
+        this.toastService.success("Cadastro realizado com sucesso!")
+        this.router.navigate([""])
+      },
+      error: (err) => {
+        const errorMessage = err.error?.message || "Erro inesperado! Tente novamente mais tarde";
+        this.toastService.error(errorMessage);
+      }
+    })
+  }
+
+  navigate(){
+    this.router.navigate([""])
+  }
+}
